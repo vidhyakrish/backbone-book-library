e332fd114d5a93453e3ae7d5d4ca780bc515bcfa
vidhyakrish <vidhyaakrish@gmail.com>
2017-08-07 11:09:25 +0100

    Update bookCollection.js

--- js/collections/bookCollection.js
+++ js/collections/bookCollection.js
@@ -4,7 +4,7 @@ define([
 ], function($, _, Backbone, BookModel) {
   var bookCollection = Backbone.Collection.extend({
     model: BookModel,
-    url: 'http://wc4042gsd.emea.ad.jpmorganchase.com:3000/library',
+    url: 'localhost:3000/library',
     save: function() {
       Backbone.sync('Add new', this, {
         success: function() {
